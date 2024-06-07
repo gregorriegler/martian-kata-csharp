066eb5b377c960f7d59c8d9c6c75f55820639a6b
Gregor Riegler <rieglerg85@gmail.com>
2024-06-07 20:46:03 +0200

    wip

--- MartianTest/UnitTest1.cs
+++ MartianTest/UnitTest1.cs
@@ -7,7 +7,7 @@ public class Tests
     [Test]
     public void AppleSauce()
     {
-        var H = ((char)int.Parse("4" + "8", System.Globalization.NumberStyles.HexNumber)).ToString();
+        var H = ((char)int.Parse("" + 4 + 8, System.Globalization.NumberStyles.HexNumber)).ToString();
         var howAlive = H + "OW ALIVE?";
         Assert.AreEqual("HOW ALIVE?", howAlive);
     }
